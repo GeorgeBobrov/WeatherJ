7023ad2959ce81a83df4cc4c6ca5077de5ad61f2
George Bobrov <skivvvvy@gmail.com>
2021-12-06 18:46:03 +0200

    Add forecast. Add city selection. Fix to hide keyboard at start. Fix for scrolling to work

--- app/src/main/java/com/example/weatherj2/MainActivity.java
+++ app/src/main/java/com/example/weatherj2/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.weatherj2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 //import android.util.Log;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Date;
@@ -25,6 +28,8 @@ import okhttp3.Response;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
+import java.text.SimpleDateFormat;
+
 public class MainActivity extends AppCompatActivity {
     static final String baseURLRemote = "http://api.openweathermap.org/data/2.5/";
     static final String APIkey = "534e27824fc3e9e6b42bd9076d595c84";
@@ -32,22 +37,37 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textResponse;
     Button buttonQuery;
+    EditText editCity;
+    LinearLayout linearLayoutH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editCity = findViewById(R.id.editCity);
         buttonQuery = findViewById(R.id.buttonQuery);
         textResponse = findViewById(R.id.textResponse);
+        linearLayoutH = findViewById(R.id.linearLayoutH);
+
+        editCity.clearFocus();
+        //hides keyboard on start
+//        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        //needs for scrolling to work
+        textResponse.setMovementMethod(new ScrollingMovementMethod());
     }
 
 
     private final OkHttpClient client = new OkHttpClient();
 
     public void buttonQueryClick(View sender) throws Exception {
+        editCity.clearFocus();
+        //hide keyboard
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(linearLayoutH.getWindowToken(), 0);
+
         HttpUrl.Builder urlBuilder = HttpUrl.parse(baseURLRemote + "weather").newBuilder();
-        urlBuilder.addQueryParameter("q", inputCity);
+        urlBuilder.addQueryParameter("q", editCity.getText().toString());
         urlBuilder.addQueryParameter("appid", APIkey);
         String url = urlBuilder.build().toString();
 
@@ -70,16 +90,15 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        processWeather(myResponse);
+                        processWeatherCity(myResponse);
                     }
                 });
-
             }
         });
 
     }
 
-    public void processWeather(String weather) {
+    public void processWeatherCity(String weather) {
         Gson gson = new Gson();
 
         try {
@@ -91,12 +110,77 @@ public class MainActivity extends AppCompatActivity {
                     weatherCity.name + " is " + normalizeTemp(weatherCity.main.temp) + " °C, " + weatherDescription;
 
             textResponse.setText(innerText);
-//            textResponse.
+//            textResponse.append("\n Humidity " + weatherCity.main.humidity);
+
+            queryWeatherForecast(weatherCity);
         } catch (JsonSyntaxException e) {
             Log.d("JsonSyntaxException", e.getLocalizedMessage());
         }
 
+    }
 
+    public void queryWeatherForecast(ResponseWeatherCity a_weatherCity) {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(baseURLRemote + "onecall").newBuilder();
+        urlBuilder.addQueryParameter("lat", a_weatherCity.coord.lat.toString());
+        urlBuilder.addQueryParameter("lon", a_weatherCity.coord.lon.toString());
+        urlBuilder.addQueryParameter("appid", APIkey);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        processWeatherForecast(myResponse);
+                    }
+                });
+            }
+        });
+    }
+
+    public void processWeatherForecast(String weather) {
+        Gson gson = new Gson();
+
+        try {
+            ResponseWeatherForecast weatherForecast = gson.fromJson(weather, ResponseWeatherForecast.class);
+
+//            Date time = new Date(weatherCity.dt * 1000l);
+
+            textResponse.append("\n Hourly:");
+
+            for (Hourly hourForecast : weatherForecast.hourly) {
+                String innerText = addHourForecast(hourForecast);
+                textResponse.append("\n" + innerText);
+            }
+        } catch (JsonSyntaxException e) {
+            Log.d("JsonSyntaxException", e.getLocalizedMessage());
+        }
+
+    }
+
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+    String addHourForecast(Hourly hourForecast) {
+        String weatherDescription = hourForecast.weather[0].main;
+        Date time = new Date(hourForecast.dt * 1000l);
+
+        String innerText = sdf.format(time) +
+                ": Temperature " + normalizeTemp(hourForecast.temp) + " °C, " +
+                weatherDescription;
+        return innerText;
     }
 
     public float normalizeTemp(float t) {
